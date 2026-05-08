7fb758ddafb14f37ae4f6fb4125734ef8f58ebd7
bwolf <bwolf10101@gmail.com>
2026-05-08 22:00:07 +0700

    learn python and make tools remove folder

---
+++ remove-folder.py
@@ -0,0 +1,14 @@
+import shutil
+
+print("Paste ur path folder for delete")
+remove = input("paste path: ").strip().strip('"')
+
+delete = input("are u sure? (y/n): ").lower()
+
+if delete == 'yes' :
+    print("clear remove..>")
+    shutil.rmtree(remove)
+elif delete == 'n' :
+    print("cansle remove")
+else :
+    print("what ar u doing? input y or n dude")
